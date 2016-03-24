4d61d5d55acef4430ebe737a4daa2dfc096e2632
KyleBWilson49 <KyleBWilson49@gmail.com>
2016-03-24 15:22:15 -0700

    initial late commit

--- src/App.js
+++ src/App.js
@@ -1,4 +1,10 @@
-import React, { Component } from 'react';
+import React, { Component } from 'react'
+import { createStore } from 'redux'
+import todoApp from './reducers/reducer'
+import { addTodo, completeTodo, setVisibilityFilter, VisibilityFilters } from './actions/actions'
+
+
+let store = createStore(todoApp)
 
 export default class App extends Component {
   render() {
